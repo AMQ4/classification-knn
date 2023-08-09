43e962ccd6230f368f8f5cef27583e858178ee53
AMQ4 <amq4319@gmail.com>
2023-08-10 00:14:26 +0300

    add an example on 'iris' dataset

--- main.cpp
+++ main.cpp
@@ -1,17 +1,15 @@
-#include "KNN.cpp"
+#include "KNN.h"
 
 using namespace std;
 using dataType = Dataset::DataType;
 int main()
 {
-KNN knn("./data/iris.csv", "species", 1);
+    Dataset mommy("./data/iris.csv"), train, test;
 
-    //vector<Dataset::DataType> x = {4.8,3.0,1.4,0.1,"Iris-setosa"};
-    auto x = knn.get_dataset().iterrow(0);
-    for (auto &&i : x)
-    {
-        cout << i << ", ";
-    }
-cout << endl;    
-    cout << knn.predict(x);
+    mommy.set_label("species");
+    mommy.split(train, test);
+
+    KNN knn(train, 1);
+
+    knn.evaluate(test);
 }
